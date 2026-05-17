c371c6286a17e03aa50679f0582d9fc02383cbb0
marcos261193 <marketingfrutifero@gmail.com>
2026-05-16 22:45:15 -0300

    supabase connected

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
+const supabase = require('./supabase');
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+app.get('/test-supabase', async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from('test')
+      .select('*');
+
+    if (error) {
+      return res.status(500).json(error);
+    }
+
+    res.json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
